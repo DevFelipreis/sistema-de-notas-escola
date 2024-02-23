58be19d4c71248f820f9d05e1346827430390861
DevFelipreis <lfreis.contato@gmail.com>
2024-02-23 12:41:26 -0300

    feat:class Usuario

---
+++ src/class/Usuario.ts
@@ -0,0 +1,22 @@
+export abstract class Usuario {
+    nome: string
+    email: string
+    protected senha: string
+    protected logado: boolean
+
+    constructor(nome: string, email: string, senha: string) {
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+        this.logado = false
+    }
+
+    login(senha: string): void {
+        if (this.email === senha) {
+            this.logado = true
+        } else {
+            this.logado = false
+            return
+        }
+    }
+}
